29ca15d2ab0185e9c166303ee0a277fbb6336917
Fahreza Pasha Haikal <abufahreza@gmail.com>
2023-05-31 22:16:59 +0700

    second commit

--- dist/js/index.js
+++ dist/js/index.js
@@ -118,3 +118,10 @@ $('a[href^="#"]').on("click", function (event) {
 });
 
 
+function showContent(contentId) {
+  var contents = document.getElementsByClassName('content');
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = 'none';
+  }
+  document.getElementById(contentId).style.display = 'block';
+}
